e5017278710f3cfb8701bb6e70b5fbc1e8827b70
Kearg <62534795+CristianIRP@users.noreply.github.com>
2024-01-25 17:33:33 -0300

    Add files via upload

---
+++ Project_Transporte-main/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        fetch('/send-email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams(formData).toString()
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al enviar el formulario');
+            }
+            return response.text();
+        })
+        .then(data => {
+            console.log(data);
+            // Puedes agregar lógica adicional aquí si es necesario
+        })
+        .catch(error => {
+            console.error(error);
+            // Puedes manejar el error según tus necesidades
+        });
+    });
+});
